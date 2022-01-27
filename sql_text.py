66311715cd52fec637541fc222dfd81828ad749b
Kaynar <Emre.Kaynar@avl.com>
2022-01-27 14:27:51 +0300

    pep8

--- sql_text.py
+++ sql_text.py
@@ -1,21 +1,21 @@
 from sqlalchemy.sql import text
-
-
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
-engine = create_engine('sqlite:///college.db', echo = True)
+
+
+engine = create_engine('sqlite:///college.db', echo=True)
 meta = MetaData()
 
 students = Table(
-   'students', meta, 
-   Column('id', Integer, primary_key = True), 
-   Column('name', String), 
-   Column('lastname', String), 
+    'students', meta,
+    Column('id', Integer, primary_key=True),
+    Column('name', String),
+    Column('lastname', String),
 )
 
 conn = engine.connect()
 
 s = text("select students.name, students.lastname from students where students.name between :x and :y")
-result = conn.execute(s, x = 'A', y = 'L').fetchall()
+result = conn.execute(s, x='A', y='L').fetchall()
 
 for row in result:
-   print (row)
+    print(row)

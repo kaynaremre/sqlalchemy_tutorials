bcd5d3bda821229361f807b54d2b7490c5c73ba9
Kaynar <Emre.Kaynar@avl.com>
2022-01-27 11:21:26 +0300

    check pep8 compliance

--- create_table.py
+++ create_table.py
@@ -1,11 +1,11 @@
 from sqlalchemy import Table, Column, Integer, String, MetaData, create_engine
 
 
-# MetaData Object is a collection of Table objects and their associated schema constructs. 
+# MetaData Object is a collection of Table objects and their associated schema constructs.
 # It holds a collection of Table objects as well as an optional binding to an Engine or Connection.
 meta = MetaData()
 
-#Create an engine for database
+# Create an engine for database
 engine = create_engine("sqlite:///college.db", echo=True)
 
 students = Table(
@@ -20,12 +20,12 @@ meta.create_all(engine)
 
 
 ins = students.insert()
-ins = students.insert().values(name = 'Emre', lastname = 'Kaynar')
+ins = students.insert().values(name='Emre', lastname='Kaynar')
 conn = engine.connect()
 result = conn.execute(ins)
 
 conn.execute(students.insert(), [
-   {'name':'Emre', 'lastname' : 'Acir'},
-   {'name':'Eren','lastname' : 'Mungan'},
-   {'name':'Ziya','lastname' : 'Ercan'},
+    {'name': 'Emre', 'lastname': 'Acir'},
+    {'name': 'Eren', 'lastname': 'Mungan'},
+    {'name': 'Ziya', 'lastname': 'Ercan'},
 ])
